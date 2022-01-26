2dc6100e1edf74e4c66cf0e052ef5c8aa8b86bf4
Jordy Felipe <31081481+jordyfelipe@users.noreply.github.com>
2022-01-26 17:26:47 -0300

    Método validarCpf, classe EntradaIncorretaException

--- src/devinmoney/entidades/Conta.java
+++ src/devinmoney/entidades/Conta.java
@@ -4,11 +4,15 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import devinmoney.exceptions.EntradaIncorretaException;
 
 public abstract class Conta {
 
 	protected String nome;
-	protected Integer cpf;
+	protected String cpf;
 	protected Double rendaMensal;
 	protected Integer conta;
 	protected Agencia agencia;
@@ -18,10 +22,10 @@ public abstract class Conta {
 	protected String extrato = "Extrato: Data | Hora | Descricao | Valor:\n";
 	protected List<Transacao> transacoes = new ArrayList<Transacao>();
 
-	public Conta(String nome, Integer cpf, Double rendaMensal, Integer conta, Agencia agencia, Double saldo,
-			Integer senha) {
+	public Conta(String nome, String cpf, Double rendaMensal, Integer conta, Agencia agencia, Double saldo,
+			Integer senha) throws EntradaIncorretaException {
 		this.nome = nome;
-		this.cpf = cpf;
+		this.cpf = setCpf(cpf);
 		this.rendaMensal = rendaMensal;
 		this.conta = conta;
 		this.agencia = agencia;
@@ -37,12 +41,22 @@ public abstract class Conta {
 		this.nome = nome;
 	}
 
-	public Integer getCpf() {
+	public String getCpf() {
 		return cpf;
 	}
 
-	public void setCpf(Integer cpf) {
-		this.cpf = cpf;
+	public String validarCpf(String cpf) throws EntradaIncorretaException {
+		Pattern pattern = Pattern.compile("[0-9]{3}\\.?[0-9]{3}\\.?[0-9]{3}-?[0-9]{2}");
+		Matcher matcher = pattern.matcher(cpf);
+		if (matcher.matches()) {
+			return cpf;
+		} else {
+			throw new EntradaIncorretaException("CPF inválido!");
+		}
+	}
+
+	public String setCpf(String cpf) throws EntradaIncorretaException {
+			return validarCpf(cpf);	
 	}
 
 	public Double getRendaMensal() {
---
+++ src/devinmoney/exceptions/EntradaIncorretaException.java
@@ -0,0 +1,10 @@
+package devinmoney.exceptions;
+
+public class EntradaIncorretaException extends Exception {
+	private static final long serialVersionUID = 1L;
+
+	public EntradaIncorretaException(String message) {
+		super(message);
+	}
+
+}
